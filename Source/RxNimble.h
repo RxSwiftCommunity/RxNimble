bc55ec8c43398dcbddbdf36bc8690e0649f35ecb
Ming Chen <Ming_Chen1@symantec.com>
2017-10-05 15:57:50 -0700

    Add RxNimble workspace and shared scheme to support carthage

---
+++ Source/RxNimble.h
@@ -0,0 +1,19 @@
+//
+//  RxNimble.h
+//  RxNimble
+//
+//  Created by Ming Chen on 10/5/17.
+//  Copyright © 2017 RxNimble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxNimble.
+FOUNDATION_EXPORT double RxNimbleVersionNumber;
+
+//! Project version string for RxNimble.
+FOUNDATION_EXPORT const unsigned char RxNimbleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxNimble/PublicHeader.h>
+
+
