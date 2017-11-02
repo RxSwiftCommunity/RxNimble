522ff63f7165667297edc1ba986e66ec91bdd98c
mosamer <mostafamamer@gmail.com>
2017-11-02 09:55:53 +0100

    Deprecate matcher functions implementation

--- Demo/DemoTests/DemoTests.swift
+++ Demo/DemoTests/DemoTests.swift
@@ -5,33 +5,42 @@ import RxNimble
 
 class RxNimbleTest: QuickSpec {
     override func spec() {
-
-        it("works with plain observables") {
-            let subject = ReplaySubject<String>.createUnbounded()
-            subject.onNext("Hi")
-
-            expect(subject.asObservable()) == "Hi"
+        /// A type-erased `Swift.Error` for testing purposes
+        struct AnyError: Swift.Error {
+            let message: String
+            init(_ message: String = "") {
+                self.message = message
+            }
         }
+        describe("First") {
+            it("works with plain observables") {
+                let subject = ReplaySubject<String>.createUnbounded()
+                subject.onNext("Hi")
 
-        it("only checks the first value") {
-            let subject = ReplaySubject<String>.createUnbounded()
-            subject.onNext("Hi")
-            subject.onNext("Hello")
+                expect(subject).first == "Hi"
+            }
 
-            expect(subject.asObservable()) == "Hi"
-        }
+            it("only checks the first value") {
+                let subject = ReplaySubject<String>.createUnbounded()
+                subject.onNext("Hi")
+                subject.onNext("Hello")
 
-        it("works with variables") {
-            let subject = Variable("Hi")
-            
-            expect(subject) == "Hi"
-        }
+                expect(subject).first == "Hi"
+            }
 
-        it("can use different matchers") {
-            let subject = ReplaySubject<String>.createUnbounded()
-            subject.onNext("")
+            it("can use different matchers") {
+                let subject = ReplaySubject<String>.createUnbounded()
+                subject.onNext("")
 
-            expect(subject).first.to(beEmpty())
+                expect(subject).first.to(beEmpty())
+            }
+
+            it("get first error") {
+                let subject = ReplaySubject<Any>.createUnbounded()
+                subject.onError(AnyError())
+
+                expect(subject).first.to(throwError())
+            }
         }
     }
 }
--- Source/RxNimble.swift
+++ Source/RxNimble.swift
@@ -3,6 +3,7 @@ import RxBlocking
 import Nimble
 
 // This is handy so we can write expect(o) == 1 instead of expect(o.value) == 1 or whatever.
+@available(*, deprecated, message: "use expect(o).first instead")
 public func equalFirst<T: Equatable, O: ObservableType>(_ expectedValue: T?) -> Predicate<O> where O.E == T {
     return Predicate.define { actualExpression in
         let actualValue = try actualExpression.evaluate()?.toBlocking().first()
@@ -13,6 +14,7 @@ public func equalFirst<T: Equatable, O: ObservableType>(_ expectedValue: T?) -> 
     }
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func equalFirst<T: Equatable>(_ expectedValue: T?) -> Predicate<Variable<T>> {
     return Predicate.define { actualExpression in
         let actualValue = try actualExpression.evaluate()?.value
@@ -23,6 +25,7 @@ public func equalFirst<T: Equatable>(_ expectedValue: T?) -> Predicate<Variable<
     }
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func equalFirst<T: Equatable, O: ObservableType>(_ expectedValue: T?) -> Predicate<O> where O.E == T? {
     return Predicate.define { actualExpression in
         let actualValue = try actualExpression.evaluate()?.toBlocking().first()
@@ -40,6 +43,7 @@ public func equalFirst<T: Equatable, O: ObservableType>(_ expectedValue: T?) -> 
     }
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func equalFirst<T: Equatable>(_ expectedValue: T?) -> Predicate<Variable<T?>> {
     return Predicate.define { actualExpression in
 
@@ -59,26 +63,33 @@ public func equalFirst<T: Equatable>(_ expectedValue: T?) -> Predicate<Variable<
 }
 
 // Applies to Observables of T, which must conform to Equatable.
+
+@available(*, deprecated, message: "use expect(o).first instead")
 public func ==<T: Equatable, O: ObservableType>(lhs: Expectation<O>, rhs: T?) where O.E == T {
     lhs.to(equalFirst(rhs))
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func ==<T: Equatable>(lhs: Expectation<Variable<T>>, rhs: T?) {
     lhs.to(equalFirst(rhs))
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func ==<T: Equatable, O: ObservableType>(lhs: Expectation<O>, rhs: T?) where O.E == Optional<T> {
     lhs.to(equalFirst(rhs))
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func ==<T: Equatable>(lhs: Expectation<Variable<T?>>, rhs: T?) {
     lhs.to(equalFirst(rhs))
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func ==<T: Equatable, O: Observable<T>>(lhs: Expectation<O>, rhs: T?) {
     lhs.to(equalFirst(rhs))
 }
 
+@available(*, deprecated, message: "use expect(o).first instead")
 public func ==<T: Equatable, O: Observable<Optional<T>>>(lhs: Expectation<O>, rhs: T?) {
     lhs.to(equalFirst(rhs))
 }
