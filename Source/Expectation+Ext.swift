40aedd3dc0fa1c7c76c617c87f62fa79bb79f6ab
Marcel Jackwerth <marceljackwerth@gmail.com>
2018-06-05 13:19:21 +0200

    Use Expectation constructor (fixes #30)

--- Source/Expectation+Ext.swift
+++ Source/Expectation+Ext.swift
@@ -1,12 +1,8 @@
 import Nimble
 
-extension Expectation {
-    init(_ expression: Expression<T>) {
-        self.expression = expression
-    }
-
-    internal func transform<U>(_ closure: @escaping (T?) throws -> U?) -> Expectation<U>{
+internal extension Expectation {
+    func transform<U>(_ closure: @escaping (T?) throws -> U?) -> Expectation<U> {
         let exp = expression.cast(closure)
-        return Expectation<U>(exp)
+        return Expectation<U>(expression: exp)
     }
 }
