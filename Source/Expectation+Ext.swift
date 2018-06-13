22b66b98334c2fdb1873fc1803672e16583791da
Marcel Jackwerth <marcel.jackwerth@fitbit.com>
2018-06-13 15:59:54 +0200

    Make change backwards compatible

--- Source/Expectation+Ext.swift
+++ Source/Expectation+Ext.swift
@@ -1,8 +1,18 @@
 import Nimble
 
 internal extension Expectation {
+    #if swift(<=4.1)
+    init(_ expression: Expression<T>) {
+        self.expression = expression
+    }
+    #endif
+
     func transform<U>(_ closure: @escaping (T?) throws -> U?) -> Expectation<U> {
         let exp = expression.cast(closure)
+        #if swift(<=4.1)
+        return Expectation<U>(exp)
+        #else
         return Expectation<U>(expression: exp)
+        #endif
     }
 }
