2c0e4f06542b8ef2282bf253e1ee4b5c884704e5
Marcel Jackwerth <marcel.jackwerth@fitbit.com>
2018-06-13 16:04:02 +0200

    Forgot that you can only use >=

--- Source/Expectation+Ext.swift
+++ Source/Expectation+Ext.swift
@@ -1,18 +1,19 @@
 import Nimble
 
 extension Expectation {
-    #if swift(<=4.1)
+    #if swift(>=4.1)
+    #else
     init(_ expression: Expression<T>) {
         self.expression = expression
     }
     #endif
 
-    internal func transform<U>(_ closure: @escaping (T?) throws -> U?) -> Expectation<U> {
+    internal func transform<U>(_ closure: @escaping (T?) throws -> U?) -> Expectation<U>{
         let exp = expression.cast(closure)
-        #if swift(<=4.1)
-        return Expectation<U>(exp)
-        #else
+        #if swift(>=4.1)
         return Expectation<U>(expression: exp)
+        #else
+        return Expectation<U>(exp)
         #endif
     }
 }
