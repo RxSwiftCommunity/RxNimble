67e9eb1ab51391b46e3544b6e9806c8a3a2b21df
Marcelo Gobetti <mwgobetti@gmail.com>
2018-10-21 22:15:43 -0300

    Added tests for RxTest

---
+++ Demo/DemoTests/RxNimbleRxTestTests.swift
@@ -0,0 +1,106 @@
+import Quick
+import Nimble
+import RxSwift
+import RxTest
+import RxNimble
+
+class RxNimbleRxTestTests: QuickSpec {
+    override func spec() {
+        describe("Events") {
+            let initialClock = 0
+            var scheduler: TestScheduler!
+            var disposeBag: DisposeBag!
+
+            beforeEach {
+                disposeBag = DisposeBag()
+                scheduler = TestScheduler(initialClock: initialClock, simulateProcessingDelay: false)
+            }
+
+            it("works with uncompleted streams") {
+                let subject = scheduler.createHotObservable([
+                    next(5, "Hello"),
+                    next(10, "World"),
+                    ])
+
+                expect(subject).events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.next(5, "Hello"),
+                        Recorded.next(10, "World")
+                        ]))
+            }
+
+            it("works with completed streams") {
+                let subject = scheduler.createHotObservable([
+                    next(5, "Hello"),
+                    next(10, "World"),
+                    completed(100)
+                    ])
+
+                expect(subject).events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.next(5, "Hello"),
+                        Recorded.next(10, "World"),
+                        Recorded.completed(100)
+                        ]))
+            }
+
+            it("works with errored streams") {
+                let subject: TestableObservable<String> = scheduler.createHotObservable([
+                    error(5, AnyError.any)
+                    ])
+
+                expect(subject).events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.error(5, AnyError.any)
+                        ]))
+            }
+
+            it("throws error if any event is error") {
+                let subject = scheduler.createHotObservable([
+                    Recorded.next(5, "Hello"),
+                    Recorded.next(10, "World"),
+                    error(15, AnyError.any)
+                    ])
+
+                expect(subject).events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(throwError())
+            }
+
+            it("does not throw error if no errors") {
+                let subject = scheduler.createHotObservable([
+                    Recorded.next(5, "Hello"),
+                    Recorded.next(10, "World")
+                    ])
+
+                expect(subject).events(scheduler: scheduler, disposeBag: disposeBag)
+                    .toNot(throwError())
+            }
+
+            it("subscribes at specified initial time") {
+                let initialTime = 50
+                let eventTime = 100
+                let subject = scheduler.createColdObservable([
+                    next(eventTime, "Hi")
+                    ])
+
+                expect(subject).events(scheduler: scheduler, disposeBag: disposeBag, startAt: initialTime)
+                    .to(equal([
+                        Recorded.next(initialTime + eventTime, "Hi")
+                        ]))
+            }
+
+            it("ignores hot stream events before initial time") {
+                let subject = scheduler.createHotObservable([
+                    next(5, "Hello"),
+                    next(10, "World"),
+                    completed(100)
+                    ])
+
+                expect(subject).events(scheduler: scheduler, disposeBag: disposeBag, startAt: 15)
+                    .to(equal([
+                        Recorded.completed(100)
+                        ]))
+            }
+        }
+    }
+}
---
+++ Source/RxTest/ThrowError+RxTest.swift
@@ -0,0 +1,36 @@
+import Nimble
+import RxSwift
+import RxTest
+
+public func throwError<T: Equatable>() -> Predicate<RecordedEvents<T>> {
+    func extractError(_ recorded: RecordedEvents<T>?) -> [Error]? {
+        func extractError<E>(_ recorded: Recorded<Event<E>>) -> Error? {
+            return recorded.value.error
+        }
+
+        #if swift(>=4.1)
+        return recorded?.compactMap(extractError)
+        #else
+        return recorded?.flatMap(extractError)
+        #endif
+    }
+
+
+    return Predicate { actualEvents in
+        var actualError: Error?
+        do {
+            let recordedEvents = try actualEvents.evaluate()
+            if let error = extractError(recordedEvents)?.first {
+                throw error
+            }
+        } catch {
+            actualError = error
+        }
+
+        if let actualError = actualError {
+            return PredicateResult(bool: true, message: .expectedCustomValueTo("throw any error", "<\(actualError)>"))
+        } else {
+            return PredicateResult(bool: false, message: .expectedCustomValueTo("throw any error", "no error"))
+        }
+    }
+}
