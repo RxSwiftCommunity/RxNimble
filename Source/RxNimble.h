1a4496eea037a276ca0ff3235aa2658519da768b
Marcelo Gobetti <mwgobetti@gmail.com>
2018-10-29 22:33:07 -0300

    Added support to Carthage
    Carthage requires a shared scheme, so now we have an actual project
    replacing the old Demo one.
    
    Initially this is going to be a single RxNimble target which will
    compile all dependencies. Let's improve this later by having specific
    RxBlocking vs. RxTest targets.
    
    Had to disable bitcode in order to successfully import XCTest (which is
    done inside Nimble).

---
+++ Source/RxNimble.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxNimble.
+FOUNDATION_EXPORT double RxNimbleVersionNumber;
+
+//! Project version string for RxNimble.
+FOUNDATION_EXPORT const unsigned char RxNimbleVersionString[];
