9e8fa5a6a0817eace7fa0478aeb0f9a762293263
xaver.lutz <xaver.lutz@saitow.ag>
2023-01-18 11:30:54 +0100

    Update Package.swift & set deployment target to required version of Nimble

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "RxNimble",
     platforms: [
-        .macOS(.v10_12), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
+        .iOS(.v13), .tvOS(.v11), .watchOS(.v4), .macOS(.v10_15)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -15,12 +15,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "10.0.0"))
+        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "11.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "RxNimble", dependencies: ["RxSwift", "Nimble", "RxTest", "RxBlocking"], path: "Source/"),
+        .target(name: "RxNimble",
+                dependencies: ["RxSwift",
+                               "Nimble",
+                               .product(name: "RxTest", package: "RxSwift"),
+                               .product(name: "RxBlocking", package: "RxSwift")],
+                path: "Source/")
     ],
     swiftLanguageVersions: [.v5]
 )
